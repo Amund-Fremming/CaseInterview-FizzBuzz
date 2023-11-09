a0ece0ee8375154cca223650bbad8e472a125597
Amund Fremming <114174150+Amund-Fremming@users.noreply.github.com>
2023-11-09 15:38:11 +0100

    Added folder structure

---
+++ src/interface/IFizzBuzzRule.cs
@@ -0,0 +1,20 @@
+/// <summary>
+/// Defines a rule for determing if a number meets a certain condition
+/// Produces a string as output for the number if the condition is met
+/// </summary>
+public interface IFizzBuzzRule
+{
+    /// <summary>
+    /// Returns true if the given number matches the methods condition (Rule).
+    /// </summary>
+    /// <param name="number">The Integer for testing if the condition is met</param>
+    /// <returns>
+    bool DivisibleBy(int number);
+    
+    /// <summary>
+    /// Produces a string as output for the rule.
+    /// Made for returning a string when the DivisibleBy condition is met.
+    /// </summary>
+    /// <returns> A string as output for the rule
+    string GetOutput();
+}
