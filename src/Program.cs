ef0ee2d1e01cffd3d631c6679f589ef007b26c31
amund-fremming <amund.fremming@gmail.com>
2023-11-10 17:52:43 +0100

    Added the first draft

--- src/Program.cs
+++ src/Program.cs
@@ -1,27 +1,25 @@
 using System;
 
-namespace Main
+namespace Main;
+class Prorgam
 {
-    class Prorgam
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        for(int i = 0; i < 100; i++)
         {
-            for(int i = 0; i < 100; i++)
+            if(i % 3 == 0 && i % 5 == 0)
             {
-                if(i % 3 == 0 && i % 5 == 0)
-                {
-                    Console.WriteLine("FizzBuzz!");
-                }
-                else if(i % 3 == 0)
-                {
-                    Console.WriteLine("Fizz!");
-                } else if(i % 5 == 0)
-                {
-                    Console.WriteLine("Buzz!");
-                } else
-                {
-                    Console.WriteLine(i);
-                }
+                Console.WriteLine("FizzBuzz!");
+            }
+            else if(i % 3 == 0)
+            {
+                Console.WriteLine("Fizz!");
+            } else if(i % 5 == 0)
+            {
+                Console.WriteLine("Buzz!");
+            } else
+            {
+                Console.WriteLine(i);
             }
         }
     }
