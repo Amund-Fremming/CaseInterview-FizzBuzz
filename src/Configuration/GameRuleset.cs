1c2bd5d872c0a688fdcd60c5c9345d0cd2c23929
amund-fremming <amund.fremming@gmail.com>
2023-11-30 09:16:45 +0100

    Trying to restore branch

--- src/Configuration/GameRuleset.cs
+++ src/Configuration/GameRuleset.cs
@@ -4,7 +4,6 @@ namespace Configuration;
 
 public class GameRuleset
 {
-    public string Name { get; set; }
     public int NumberOfIterations { get; set; }
     public bool Ascending { get; set; }
     public List<Rule> Rules { get; set; } = new List<Rule>();
--- src/Game/Games.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Configuration;
-
-namespace Game;
-
-public class Games {
-
-    public List<GameRuleset> GameCollection { get; set; } = new List<GameRuleset>(); 
-}
