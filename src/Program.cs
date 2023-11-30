36d4c90973d97c675c95ca3f146da94231b28b31
amund-fremming <amund.fremming@gmail.com>
2023-11-30 09:18:45 +0100

    Trying to restore branch

--- src/Program.cs
+++ src/Program.cs
@@ -2,23 +2,38 @@
 using Configuration;
 using Microsoft.Extensions.Configuration;
 
-Games CreateGameRuleset(string gameset) {
+GameRuleset CreateGameRuleset(string rulesetKey) {
     var configuration = new ConfigurationBuilder()
         .AddJsonFile("Configuration/rules.json", optional: false, reloadOnChange: true)
         .Build();
 
-    return configuration.GetSection(gameset).Get<Games>() ?? throw new InvalidOperationException($"Configuration section {gameset}");
+    return configuration.GetSection(rulesetKey).Get<GameRuleset>() ?? throw new InvalidOperationException($"Configuration section {rulesetKey}");
 }
 
-void PlayGame(Games games)
+void PlayGame(GameRuleset gameRuleset)
 {
-    GameLogic gameLogic = new GameLogic(games);
-    gameLogic.PlayGames();
+    GameLogic gameLogic = new GameLogic(gameRuleset.Rules);
+
+    IEnumerable<int> interval = Enumerable.Range(
+        Math.Min(gameRuleset.IntervalStart, gameRuleset.IntervalEnd),
+        Math.Abs(gameRuleset.IntervalEnd - gameRuleset.IntervalStart) + 1
+    );
+
+    bool descendingInterval = gameRuleset.IntervalStart > gameRuleset.IntervalEnd ? true : false;
+
+    if(descendingInterval)
+        interval = interval.Reverse();
+
+    foreach(int num in interval)
+    {
+        var outputText = gameLogic.ApplyRules(num);
+        Console.WriteLine(outputText);
+    }
 }
 
-var ruleset = CreateGameRuleset("gameset");
-Console.WriteLine(ruleset.GameCollection.Count());
+/* GAME 1 */
+PlayGame(CreateGameRuleset("fizzbuzz"));
 
-PlayGame(ruleset);
-
+/* GAME 2 */
+PlayGame(CreateGameRuleset("jazzfuzz"));
 
